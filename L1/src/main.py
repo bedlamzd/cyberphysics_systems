4eb3f54f534ebe39d28ba4aac2370bc20c7f9430
bedlamzd <bedlamzd@gmail.com>
2021-02-16 22:27:30 +0300

    L1 report done

--- L1/src/main.py
+++ L1/src/main.py
@@ -3,20 +3,12 @@ from typing import Tuple
 import numpy as np
 import matplotlib.pyplot as plt
 
-img_dir = 'L1/img/'
 
-data_path = 'L1/data/testLab1Var6.csv'
-columns = ['time', 'current', 'voltage']
-
-
-def estimate_params(voltage: np.ndarray, current: np.ndarray, Td: float) -> Tuple[float, float, float, np.ndarray]:
-    X = np.c_[voltage, current]
-    Y = current
-
+def estimate_params(X: np.ndarray, Y: np.ndarray, Td: float) -> Tuple[float, float, float, np.ndarray]:
     k = np.linalg.inv(X.T @ X) @ X.T @ Y
     R = (1 - k[1]) / k[0]
     Te = -Td / np.log(k[1])
-    L = T * R
+    L = Te * R
 
     return (R, L, Te, k)
 
@@ -25,6 +17,11 @@ if __name__ == '__main__':
     ## Given
     T = 0.1  # Period
     Td = 1e-3  # sampling period
+
+    img_dir = '../img/'
+    data_path = '../data/testLab1Var6.csv'
+    columns = ['time', 'current', 'voltage']
+
     data = np.genfromtxt(data_path, names=columns, delimiter=',')
 
     ## Display all data
@@ -63,15 +60,18 @@ if __name__ == '__main__':
     fig.savefig(img_dir + f'current-voltage-{N}T.png')
 
     ## Estimate parameters
-    X = np.c_[data['voltage'][1:-1], data['current'][1:-1]]
-    R, L, Te, k = estimate_params(X[:, 0], X[:, 1], Td)
+    X = np.c_[data['voltage'][:-1], data['current'][:-1]]
+    Y = data['current'][1:]
+    R, L, Te, k = estimate_params(X, Y, Td)
+    print(f'Estimated R = {R} Ohm\n'
+          f'Estimated L = {L} Hn')
 
     ## Compare model and given data
-    current_est = (X @ k)[:time.size - 1]
+    current_est = (X @ k)[:time.size]
 
     fig, ax = plt.subplots()
     ax.plot(time, current, label='Исходные данные')
-    ax.plot(time[1:], current_est, label='Аппроксимированные данные')
+    ax.plot(time, current_est, label='Аппроксимированные данные')
     ax.set(xlabel='Время, с',
            ylabel='Сила тока, А',
            title='Сравнение модели и данных')
@@ -80,22 +80,25 @@ if __name__ == '__main__':
     fig.savefig(img_dir + 'current_estimation_comparison.png')
 
     ## Mean and std of parameters
-    R_est = []
-    L_est = []
+    R_est = np.array([])
+    L_est = np.array([])
     n = 1000
     for i in range(n):
         indices = (i * T <= data['time']) & (data['time'] <= (i + 1) * T)
         curr = data['current'][indices]
         volt = data['voltage'][indices]
 
-        R, L, Te, k = estimate_params(volt[1:-1], curr[1:-1], Td)
+        X = np.c_[volt[:-1], curr[:-1]]
+        Y = curr[1:]
 
-        R_est.append(R)
-        L_est.append(L)
+        R, L, Te, k = estimate_params(X, Y, Td)
+
+        R_est = np.append(R_est, R)
+        L_est = np.append(L_est, L)
 
     print(
-            f'Mean R: {np.mean(R_est, where=np.isfinite(R_est))}, Ohm\n'
-            f'Standard deviation R: {np.std(R_est, where=np.isfinite(R_est))}\n'
-            f'Mean L: {np.mean(L_est, where=np.isfinite(L_est))}, Hn\n'
-            f'Standard deviation L: {np.std(L_est, where=np.isfinite(L_est))}\n'
+            f'Mean R: {np.mean(R_est[np.isfinite(R_est)])}, Ohm\n'
+            f'Standard deviation R: {np.std(R_est[np.isfinite(R_est)])}\n'
+            f'Mean L: {np.mean(L_est[np.isfinite(L_est)])}, Hn\n'
+            f'Standard deviation L: {np.std(L_est[np.isfinite(L_est)])}\n'
             )
